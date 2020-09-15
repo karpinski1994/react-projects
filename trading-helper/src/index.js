379d0412153c6b295918d3d5a80a6dc4112c64da
karpinski94 <patryk.karpinski@outlook.com>
2020-09-15 22:23:30 +0200

    Set up babel, webpack with presets and webpack-dev-server

---
+++ trading-helper/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import List from './containers/List'
+
+const App = () => {
+  return (
+    <div>
+      <List />
+      Trading helper x x x
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
