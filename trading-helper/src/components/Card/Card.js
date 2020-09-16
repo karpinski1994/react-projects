42bd6253933622b8a1bc1c8fc5b30e8854fea639
karpinski94 <patryk.karpinski@outlook.com>
2020-09-16 19:37:49 +0200

    Add eslint and proptypes

--- trading-helper/src/components/Card/Card.js
+++ trading-helper/src/components/Card/Card.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Card = () => <div>card</div>
 
+
+Card.prototypes = {
+  movie: PropTypes.shape({
+    
+  })
+}
 export default Card
