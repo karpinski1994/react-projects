c167dcc0e2692910b84981132860f8868bd8414d
karpinski94 <patryk.karpinski@outlook.com>
2020-09-16 19:07:20 +0200

    Add mock List container and Card component

---
+++ trading-helper/src/containers/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Card from '../components/Card/Card'
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+        <Card />
+      </div>
+    )
+  }
+}
+
+export default List
